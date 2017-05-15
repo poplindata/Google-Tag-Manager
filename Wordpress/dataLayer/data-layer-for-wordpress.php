48dc73280e14268e23158c7d422bee86518c6798
Narbeh Yousefian <narbehis@gmail.com>
2017-05-16 09:02:00 +1000

    DL 1.3 and GTM 1.1

--- Wordpress/dataLayer/data-layer-for-wordpress.php
+++ Wordpress/dataLayer/data-layer-for-wordpress.php
@@ -10,18 +10,21 @@
 // Installation and uninstallation hooks
 register_activation_hook(__FILE__, 'spcode_activate');
 register_deactivation_hook(__FILE__, 'spcode_deactivate');
+
 /*
  * Activate the plugin
  */
 function spcode_activate() {
     // Do nothing
 } // END public static function activate
+
 /*
  * Deactivate the plugin
  */
 function spcode_deactivate() {
     // Do nothing
 } // END public static function deactivate
+
 function injectSPCode() {
     global $post, $wp_query, $wpdb, $current_user;
     $post_categories = get_the_category( get_the_ID() );
@@ -36,160 +39,163 @@ function injectSPCode() {
     } else {
         $wp_visitor_type = "guest";
     }
+
+    $dl = array();
+
+    switch (true) {
+        case is_front_page():
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'front-page';
+            $dl["WPtemplateDisplayType"] = 'blog';
+            $dl["WPtemplateFileName"] = 'index';
+            $dl["WPtemplateFileId"] = intval(get_the_ID());
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_home():
+            $dl["WPtemplateFile"] = 'page';
+            $dl["WPtemplateDisplay"] = 'home';
+            $dl["WPtemplateDisplayType"] = 'home';
+            $dl["WPtemplateFileName"] = 'index';
+            $dl["WPtemplateFileId"] = '';
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_single():
+            $last_category = end($post_categories);
+            $formatted_tags = array();
+            if ( !empty($post_tags) ) {
+                foreach(@$post_tags as $tag) {
+                    $formatted_tags[] = $tag->name;
+                }
+            }
+
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'single-post';
+            $dl["WPtemplateDisplayType"] = strtolower( get_post_type( get_the_ID() ) );
+            $dl["WPtemplateFileName"] = strtolower( get_the_title() );
+            $dl["WPtemplateFileId"] = intval(get_the_ID());
+            $dl["WPpostCategory"] = $last_category->cat_name;
+            $dl["WPpostTags"] = $formatted_tags;
+            $dl["WPpostDate"] =  date( 'Y-m-d', strtotime( $post->post_date ) );
+            $dl["WPpostAuthor"] = get_the_author_meta( 'user_nicename' , $post->post_author );
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_page():
+            $dl["WPtemplateFile"] = 'page';
+            $dl["WPtemplateDisplay"] = 'page';
+            $dl["WPtemplateDisplayType"] = strtolower( $post->post_name );
+            $dl["WPtemplateFileName"] = strtolower( get_the_title() );
+            $dl["WPtemplateFileId"] = intval(get_the_ID());
+            $dl["WPpostDate"] = date( 'Y-m-d', strtotime( $post->post_date ) );
+            $dl["WPpostAuthor"] = get_the_author_meta( 'user_nicename' , $post->post_author );
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_category():
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'category';
+            $dl["WPtemplateDisplayType"] = 'archive';
+            $dl["WPtemplateFileName"] = strtolower( get_cat_name( $wp_query->query_vars['cat'] ) );
+            $dl["WPtemplateFileId"] = intval($wp_query->query_vars['cat']);
+            $dl["WPpostCountOnPage"] = intval($wp_query->post_count);
+            $dl["WPpostCountTotal"] = intval($wp_query->found_posts);
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_tag():
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'tag';
+            $dl["WPtemplateDisplayType"] = 'archive';
+            $dl["WPtemplateFileName"] = strtolower( $wp_query->query_vars['tag'] );
+            $dl["WPtemplateFileId"] = intval($wp_query->query_vars['tag_id']);
+            $dl["WPpostCountOnPage"] = intval($wp_query->post_count);
+            $dl["WPpostCountTotal"] = intval($wp_query->found_posts);
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_author():
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'author';
+            $dl["WPtemplateDisplayType"] = 'archive';
+            $dl["WPtemplateFileName"] = strtolower( get_the_author_meta( 'user_nicename' , $post->post_author ) ) . '.php';
+            $dl["WPtemplateFileId"] = intval($post->post_author);
+            $dl["WPpostCountOnPage"] = intval($wp_query->post_count);
+            $dl["WPpostCountTotal"] = intval($wp_query->found_posts);
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_date():
+            if ( is_day() ) {
+                $wp_template_file_name = "Day";
+            } else if ( is_month() ) {
+                $wp_template_file_name = "Month";
+            } else if ( is_year() ) {
+                $wp_template_file_name = "Year";
+            }
+
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'date';
+            $dl["WPtemplateDisplayType"] = 'archive';
+            $dl["WPtemplateFileName"] = strtolower( $wp_template_file_name );
+            $dl["WPtemplateFileId"] = intval(get_the_ID());
+            $dl["WPpostCountOnPage"] = intval($wp_query->post_count);
+            $dl["WPpostCountTotal"] = intval($wp_query->found_posts);
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_search():
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'search';
+            $dl["WPtemplateDisplayType"] = 'archive';
+            $dl["WPtemplateFileName"] = 'search';
+            $dl["WPtemplateFileId"] = intval(get_the_ID());
+            $dl["WPpostCountOnPage"] = intval($wp_query->post_count);
+            $dl["WPpostCountTotal"] = intval($wp_query->found_posts);
+            $dl["WPsiteSearchFrom"] = wp_get_referer();
+            $dl["WPsiteSearchResults"] = intval($wp_query->found_posts);
+            $dl["WPsiteSearchTerm"] = $wp_query->query['s'];
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+        case is_404():
+            $dl["WPtemplateFile"] = 'post';
+            $dl["WPtemplateDisplay"] = 'error';
+            $dl["WPtemplateDisplayType"] = '404';
+            $dl["WPtemplateFileName"] = strtolower( $post->post_name );
+            $dl["WPtemplateFileId"] = intval(get_the_ID());
+            $dl["WPappId"] = $wp_app_ID;
+            $dl["WPplatform"] = $wp_platform;
+            $dl["WPsiteProfile"] = $wp_site_profile;
+            $dl["WPvisitorType"] = $wp_visitor_type;
+            break;
+    }
 ?>
     <!-- dataLayer starts -->
-        <script>
-            dataLayer = [{
-                <?php
-                if ( is_front_page() ) {
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'front-page',
-                'WPtemplateDisplayType': 'blog',
-                'WPtemplateFileName': 'index',
-                'WPtemplateFileId': " . intval(get_the_ID()) . ",
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_home() ) {
-                    echo
-                    "'WPtemplateFile': 'page',
-                'WPtemplateDisplay': 'home',
-                'WPtemplateDisplayType': 'home',
-                'WPtemplateFileName': 'index',
-                'WPtemplateFileId': '',
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_single() ) {
-                    $last_category = end($post_categories);
-                    $formatted_tags = '';
-                    if ( !empty($post_tags) ) {
-                        foreach(@$post_tags as $tag) {
-                            $formatted_tags[] = "'" . $tag->name . "'";
-                        }
-                        $formatted_tags = implode( ', ', $formatted_tags );
-                    }
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'single-post',
-                'WPtemplateDisplayType': '" . strtolower( get_post_type( get_the_ID() ) ) . "',
-                'WPtemplateFileName': '" . strtolower( get_the_title() ) . "',
-                'WPtemplateFileId': " . intval(get_the_ID()) . ",
-                'WPpostCategory': '" . $last_category->cat_name . "',
-                'WPpostTags': [" . $formatted_tags . "],
-                'WPpostDate': '" . date( 'Y-m-d', strtotime( $post->post_date ) ) . "',
-                'WPpostAuthor': '" . get_the_author_meta( 'user_nicename' , $post->post_author ) . "',
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_page() ) {
-                    echo
-                    "'WPtemplateFile': 'page',
-                'WPtemplateDisplay': 'page',
-                'WPtemplateDisplayType': '" . strtolower( $post->post_name ) . "',
-                'WPtemplateFileName': '" . strtolower( get_the_title() ) . "',
-                'WPtemplateFileId': " . intval(get_the_ID()) . ",
-                'WPpostDate': '" . date( 'Y-m-d', strtotime( $post->post_date ) ) . "',
-                'WPpostAuthor': '" . get_the_author_meta( 'user_nicename' , $post->post_author ) . "',
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_category() ) {
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'category',
-                'WPtemplateDisplayType': 'archive',
-                'WPtemplateFileName': '" . strtolower( get_cat_name( $wp_query->query_vars['cat'] ) ) . "',
-                'WPtemplateFileId': " . intval($wp_query->query_vars['cat']) . ",
-                'WPpostCountOnPage': " . intval($wp_query->post_count) . ",
-                'WPpostCountTotal': " . intval($wp_query->found_posts) . ",
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_tag() ) {
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'tag',
-                'WPtemplateDisplayType': 'archive',
-                'WPtemplateFileName': '" . strtolower( $wp_query->query_vars['tag'] ) . "',
-                'WPtemplateFileId': " . intval($wp_query->query_vars['tag_id']) . ",
-                'WPpostCountOnPage': " . intval($wp_query->post_count) . ",
-                'WPpostCountTotal': " . intval($wp_query->found_posts) . ",
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_author() ) {
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'author',
-                'WPtemplateDisplayType': 'archive',
-                'WPtemplateFileName': '" . strtolower( get_the_author_meta( 'user_nicename' , $post->post_author ) ) . ".php',
-                'WPtemplateFileId': " . intval($post->post_author) . ",
-                'WPpostCountOnPage': " . intval($wp_query->post_count) . ",
-                'WPpostCountTotal': " . intval($wp_query->found_posts) . ",
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_date() ) {
-                    if ( is_day() ) {
-                        $wp_template_file_name = "Day";
-                    } else if ( is_month() ) {
-                        $wp_template_file_name = "Month";
-                    } else if ( is_year() ) {
-                        $wp_template_file_name = "Year";
-                    }
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'date',
-                'WPtemplateDisplayType': 'archive',
-                'WPtemplateFileName': '" . strtolower( $wp_template_file_name ) . "',
-                'WPtemplateFileId': " . intval(get_the_ID()) . ",
-                'WPpostCountOnPage': " . intval($wp_query->post_count) . ",
-                'WPpostCountTotal': " . intval($wp_query->found_posts) . ",
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_search() ) {
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'search',
-                'WPtemplateDisplayType': 'archive',
-                'WPtemplateFileName': 'search',
-                'WPtemplateFileId': " . intval(get_the_ID()) . ",
-                'WPpostCountOnPage': " . intval($wp_query->post_count) . ",
-                'WPpostCountTotal': " . intval($wp_query->found_posts) . ",
-                'WPsiteSearchFrom': '" . wp_get_referer() . "',
-                'WPsiteSearchResults': " . intval($wp_query->found_posts) . ",
-                'WPsiteSearchTerm': '" . $wp_query->query['s'] . "',
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                } else if ( is_404() ) {
-                    echo
-                    "'WPtemplateFile': 'post',
-                'WPtemplateDisplay': 'error',
-                'WPtemplateDisplayType': '404',
-                'WPtemplateFileName': '" . strtolower( $post->post_name ) . "',
-                'WPtemplateFileId': " . intval(get_the_ID()) . ",
-                'WPappId': '" . $wp_app_ID . "',
-                'WPplatform': '" . $wp_platform . "',
-                'WPsiteProfile': '" . $wp_site_profile . "',
-                'WPvisitorType': '" . $wp_visitor_type . "'";
-                }
-                ?>
-
-            }];
-        </script>
+    <script>
+        dataLayer = [<?php
+            echo json_encode($dl, JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_PARTIAL_OUTPUT_ON_ERROR);
+            ?>];
+    </script>
     <!-- dataLayer stops -->
 <?php
 }
